4b2d0c039fa7518dbf966518f33e6c98c13ad87d
Dinesh Ram Kumar Thandapani <tdrk@uber.com>
2021-06-07 20:23:39 +0530

    Added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "v1";
+
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+// install service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match(event.request));
+    })
+  );
+});
+
+// activate service worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+          return Promise.resolve();
+        })
+      )
+    )
+  );
+});
